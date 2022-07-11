39e4195b52471084fde5162fda68bf8167e875ee
Benjamin Frodsham <bfrodsham@gmail.com>
2022-07-11 16:00:43 -0600

    Initial Commit

---
+++ src/main/java/com/example/householdorganizer/HouseholdOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.householdorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/householdorganizer/HouseholdOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.householdorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
