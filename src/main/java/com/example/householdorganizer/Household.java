b11160938799153b6534a1d9bdc2ff538626203b
Benjamin Frodsham <bfrodsham@gmail.com>
2022-07-12 15:24:28 -0600

    Added classes, finished Residence class.

---
+++ src/main/java/com/example/householdorganizer/Household.java
@@ -0,0 +1,4 @@
+package com.example.householdorganizer;
+
+public class Household {
+}
---
+++ src/main/java/com/example/householdorganizer/Residence.java
@@ -0,0 +1,53 @@
+package com.example.householdorganizer;
+import org.apache.commons.text.WordUtils;
+
+
+public class Residence {
+
+    private final String address;
+    private final String city;
+    private final String state;
+
+    private final String fullAddress;
+
+    public Residence(String address, String city, String state) {
+
+        this.address = standardizeResidentInput(address);
+        this.city = standardizeResidentInput(city);
+
+        // I'm making the assumption that state input will always be two letters with no punctuation.
+        this.state = state.toUpperCase();
+
+        this.fullAddress = this.address + ", " + this.city + ", " + this.state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    // This function standardizes resident fields so that comparison
+    // between full addresses can be done later.
+    private String standardizeResidentInput(String location) {
+
+        // First, the location string is put in lowercase, and then
+        // WordUtils capitalizes the first character in each word.
+        WordUtils.capitalize(location.toLowerCase());
+
+        // Punctuation is removed. The regex used means something like
+        // "anything that is not an uppercase or lowercase letter."
+        return location.replaceAll("[^a-zA-Z ]", "");
+    }
+
+}
